80006b5e9446da984fc7c2768aa532a7d668d16c
Aayush Pandey <aayushpandey1100@gmail.com>
2022-08-22 13:00:49 +0530

    Layout - Done

--- app/src/main/java/com/codeeraayush/notesapp_mvvm/MainActivity.java
+++ app/src/main/java/com/codeeraayush/notesapp_mvvm/MainActivity.java
@@ -2,13 +2,26 @@ package com.codeeraayush.notesapp_mvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.codeeraayush.notesapp_mvvm.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.newNotesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,InsertNotes.class));
+            }
+        });
+
     }
 }
